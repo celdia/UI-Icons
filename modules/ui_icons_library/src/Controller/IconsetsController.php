6aefdfd2c27fb8349e261c0c2fa60a24d9d30cb1
Cellou Diallo <celdiatech@gmail.com>
2024-06-06 19:01:00 +0200

    Overview and single page of icons library

---
+++ modules/ui_icons_library/src/Controller/IconsetsController.php
@@ -0,0 +1,70 @@
+<?php
+
+
+namespace Drupal\ui_icons_library\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
+
+/**
+ * Class IconDisplayController.
+ */
+class IconsetsController extends ControllerBase
+{
+
+  const OVERVIEW_SLICE = 20;
+
+  /**
+   * Display icons.
+   *
+   * @return array
+   *   Return render array.
+   */
+  public function overview()
+  {
+    $build = [];
+    $iconsets = \Drupal::service('strategy.manager.iconset')->getIconsets();
+    foreach ($iconsets as $iconset_id => $label) {
+      $iconset = \Drupal::service('strategy.manager.iconset')->getInstance($iconset_id);
+      $icons = $iconset->getIcons();
+      $icons = array_slice($icons, 0, static::OVERVIEW_SLICE);
+      $build[] = [
+        '#type' => 'html_tag',
+        '#tag' => 'h2',
+        '#value' => $label,
+      ];
+      foreach ($icons as $icon_id => $icon) {
+        $build[] = $iconset->build($icon_id);
+      }
+      $build[] = [
+        '#type' => 'html_tag',
+        '#tag' => 'p',
+        0 => [
+          '#type' => 'link',
+          '#title' => $this->t("View more"),
+          '#url' => Url::fromRoute('ui_icons.single', ["iconset_id" => $iconset_id]),
+        ]
+      ];
+    }
+    return $build;
+  }
+
+
+  /**
+   * Display icons.
+   *
+   * @return array
+   *   Return render array.
+   */
+  public function single(string $iconset_id)
+  {
+    $build = [];
+    $iconset = \Drupal::service('strategy.manager.iconset')->getInstance($iconset_id);
+    $icons = $iconset->getIcons();
+    foreach ($icons as $icon_id => $icon) {
+        $build[] = $iconset->build($icon_id);
+      }
+    return $build;
+  }
+
+}
